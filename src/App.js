42728cb9b3094509fd20fc146bc8867d81c85660
Rodean Fraser <rodeanfraser@Yuri-Mac-Mini.local>
2019-12-18 14:43:54 -0500

    setup react app with MUI

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SampleForm from './SampleForm'
+import SampleAppBar from './SampleAppBar'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SampleAppBar />
+      <SampleForm />
+    </div>
+  );
+}
+
+export default App;
